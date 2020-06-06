eb0c733c65dca0c9bd7a2a3ce8a5ea350fc57a3a
AkihideKato <o8033998770@gmail.com>
2020-06-06 18:23:12 +0900

    edit migrate

--- db/migrate/20200531093649_create_messages.rb
+++ db/migrate/20200531093649_create_messages.rb
@@ -1,8 +1,8 @@
 class CreateMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :messages do |t|
-      t.string :name
-      t.string :text
+      t.string :title
+      t.string :text, null: false
       t.text :image
       t.timestamps
     end
