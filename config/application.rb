cfba44676af6085a341e352293612ce9eb3f13b6
AkihideKato <o8033998770@gmail.com>
2020-06-11 11:13:42 +0900

    flash message

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module StudyApp
     I18n.enforce_available_locales = false
     I18n.config.available_locales = :ja
     I18n.default_locale = :ja
+    
 
 
     # Settings in config/environments/* take precedence over those specified here.
