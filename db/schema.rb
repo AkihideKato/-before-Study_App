25a8c6b22399d9d60041911b793dadeee7215ac1
AkihideKato <o8033998770@gmail.com>
2020-06-12 12:35:10 +0900

    edit : view => message-new , devie-new , validate

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2020_06_07_012956) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
